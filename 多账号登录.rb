ac83a3edd462ce5b909e70dd310b454db39b36ff
codinghiker <62997312+codinghiker@users.noreply.github.com>
2020-05-13 20:40:48 +0800

    Create 多账号登录.rb

---
+++ 多账号登录.rb
@@ -0,0 +1,61 @@
+ spaceship/client.rb
+ 
+ 
+ # Get the `itctx` from the new (22nd May 2017) API endpoint "olympus"
+    # Update (29th March 2019) olympus migrates to new appstoreconnect API
+    def fetch_olympus_session0
+
+
+      response = request(:get, "https://appstoreconnect.apple.com/olympus/v1/session")
+      body = response.body
+      if body
+        body = JSON.parse(body) if body.kind_of?(String)
+        user_map = body["user"]
+        if user_map
+          self.user_email = user_map["emailAddress"]
+        end
+
+        provider = body["provider"]
+        if provider
+          self.provider = Spaceship::Provider.new(provider_hash: provider)
+          return true
+        end
+      end
+
+      return false
+    end
+    def fetch_olympus_session
+      get_team_id(team_id: "120015883")
+
+      response = request(:get, "https://appstoreconnect.apple.com/olympus/v1/session")
+      body = response.body
+      if body
+        body = JSON.parse(body) if body.kind_of?(String)
+        user_map = body["user"]
+        if user_map
+          self.user_email = user_map["emailAddress"]
+        end
+
+        provider = body["provider"]
+        if provider
+          self.provider = Spaceship::Provider.new(provider_hash: provider)
+          return true
+        end
+      end
+
+      return false
+    end
+    
+    
+    
+line 526    
+    
+    
+    
+          when 403
+        raise InvalidUserCredentialsError.new, "Invalid username and password combination. Used '#{user}' as the username."
+      when 200
+        fetch_olympus_session0
+        fetch_olympus_session
+        return response
+      when 409
